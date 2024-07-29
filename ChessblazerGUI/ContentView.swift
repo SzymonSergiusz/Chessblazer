0a15a54c1ba4e12ead3e99823490863db636f092
SzymonSergiusz <62117922+SzymonSergiusz@users.noreply.github.com>
2024-07-29 02:28:26 +0200

    early check validations, bug fixes

--- ChessblazerGUI/ContentView.swift
+++ ChessblazerGUI/ContentView.swift
@@ -1,17 +1,11 @@
-//
-//  ContentView.swift
-//  ChessblazerGUI
-//
-//  Created by sergiusz on 28/07/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var game = Game()
+    
     var body: some View {
         VStack {
-            Chessboard()
-       
+            Chessboard(game: $game)
         }
         .padding()
     }
@@ -19,41 +13,84 @@ struct ContentView: View {
 
 struct Square: View {
     var color: Color
+    var pieceValue: Int
+    
+    let pieceImage: [Int: Image] = [
+        18 : Image("blackpawn"),
+        21 : Image("blackrook"),
+        19 : Image("blackknight"),
+        20 : Image("blackbishop"),
+        22 : Image("blackqueen"),
+        17 : Image("blackking"),
+        10 : Image("whitepawn"),
+        13 : Image("whiterook"),
+        11 : Image("whiteknight"),
+        12 : Image("whitebishop"),
+        14 : Image("whitequeen"),
+        9 : Image("whiteking"),
+    ]
     
     var body: some View {
-        Rectangle()
-            .fill(color)
-            .frame(width: 50, height: 50)
+        ZStack {
+            Rectangle()
+                .fill(color)
+                .frame(width: 50, height: 50)
+            
+            if pieceValue != 0 {
+                pieceImage[pieceValue]?
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+            }
+        }
     }
 }
 
+
 struct Chessboard: View {
+    @Binding var game: Game
+    
     var body: some View {
         VStack(spacing: 0) {
-
             let lightBrown = Color(red: 245/255, green: 213/255, blue: 193/255)
             let brown = Color(red: 133/255, green: 75/255, blue: 40/255)
-                
-            ForEach(0..<8) { row in
+            let letters = ["a", "b", "c", "d","e","f","g","h"]
+
+            VStack(spacing: 0) {
                 HStack(spacing: 0) {
-                    ForEach(0..<8) { column in
-                        Square(color: (row + column).isMultiple(of: 2) ? lightBrown : brown)
+
+                    Text(" ")
+                        .frame(width: 50, height: 50)
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .font(.headline)
+                    ForEach(letters, id: \.self) { letter in
+                        Text(letter)
+                            .frame(width: 50, height: 50)
+                            .background(.white)
+                            .foregroundColor(.black)
+                            .font(.headline)
+                    }
+                }
+                ForEach(0..<8) { row in
+                    HStack(spacing: 0) {
+
+                        Text("\(8 - row)")
+                            .frame(width: 50, height: 50)
+                            .background(.white)
+                            .foregroundColor(.black)
+                            .font(.headline)
+                        ForEach(0..<8) { column in
+                            let index = (7 - row) * 8 + column
+                            let pieceValue = game.board[index]
+                            Square(color: (row + column).isMultiple(of: 2) ? lightBrown : brown, pieceValue: pieceValue)
+                        }
                     }
                 }
             }
         }
     }
 }
-
-struct PieceImage: View {
-    var body: some View {
-        Image(.blackpawn)
-            .resizable()
-            .scaledToFit()
-            .frame(width: 60, height: 60)
-    }
-}
-
 
 #Preview {
     ContentView()
